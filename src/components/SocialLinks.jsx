4562e580ae8ae2e51c2002124192e32523e15d08
Akash <akash@gmail.com>
2023-04-15 17:39:07 +0530

    project done

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/gh.pdf",
+      href: "/AB-Resume.pdf",
       style: "rounded-br-md",
       download: true,
     },
